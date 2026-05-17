1abf824ac48cb9d88a578ba8596fa5c2cc2bad52
casperkangas <casperkangas@gmail.com>
2026-05-17 16:51:59 +0300

    Initialize Wishlist project with essential files and configurations

---
+++ Wishlist/Wishlist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wishlist
+//
+//  Created by Casper Kangas on 17.5.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wishlist/Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by Casper Kangas on 17.5.2026.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
